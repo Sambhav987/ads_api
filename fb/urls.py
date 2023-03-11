40c765ac32f5d510c71e9653aeeb833bb7e0f131
Sambhav987 <sambhavagarwal6@gmail.com>
2023-03-11 21:53:07 +0530

    Initial commit 2

--- fb/urls.py
+++ fb/urls.py
@@ -4,7 +4,8 @@ from fb import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login/', views.index, name='login'),
+    # path('login/', views.index, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('campaigns/', views.campaigns, name='campaigns'),
     path('about/', views.about, name='about'),
 ]
--- fb/views.py
+++ fb/views.py
@@ -10,24 +10,35 @@ from .models import User
 
 from . import fbads
 
+user_data = None
+
 # Create your views here.
 def index(request):
     # if request.user.is_authenticated:
     #     return HttpResponseRedirect(reverse("dashboard"))
+    if request.method == "POST":
+        pass
+    
+    if user_data is None:
+        return render(request, 'fb/index.html')
+    else:
+        return redirect('dashboard/')
 
-    return render(request, 'fb/index.html')
-
-
-def login(request):
-    return render(request, 'fb/index.html')
+# def login(request):
+#     return render(request, 'fb/index.html')
 
 def about(request):
     return render(request, 'fb/about.html')
 
+
 def dashboard(request):
+    return render(request, "fb/dashboard.html")
+
+
+def campaigns(request):
     context = fbads.getCampaigns(request, User)
     if context is not None:
-        return render(request, "fb/dashboard.html", context)
+        return render(request, "fb/campaigns.html", context)
     
     else:
-        return redirect('/fb/login/')
+        return HttpResponseRedirect('/fb/', {"message_type" : "danger", "message" : "Error in retrieving campaigns!"})
